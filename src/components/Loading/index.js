de6ad86de5b3a2ee9bfb61bc65204617af60fc4f
Ricardo Rodriguez <tjrprecursor@gmail.com>
2019-03-25 10:37:40 -0400

    loading color changed

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,7 +4,7 @@ import { Spin, Icon } from 'antd';
 // Styles
 import './Loading.scss';
 
-const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+const antIcon = <Icon type="loading" style={{ fontSize: 24, color: '#DA674A' }} spin />;
 const Loading = () => (
   <div className="Loading">
     <Spin indicator={antIcon} tip="Loading..." size="large" />
